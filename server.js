983d8d31f8262e9ac91e25fca80580de21a73668
Vertirax <k.andras1996@gmail.com>
2022-11-11 17:58:58 +0100

    Server fix
    - fixed reload issue by using history mode

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
+const history = require("connect-history-api-fallback");
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-const options = {
-    dotfiles: "ignore",
-    etag: false,
-    index: ["index.html"],
-    redirect: false
-}
-app.use(express.static("dist", options))
+app.use(history());
+app.use(express.static(path.join(__dirname + "/dist")));
 
-app.listen(process.env.PORT || 80)
+app.listen(process.env.PORT || 80);
